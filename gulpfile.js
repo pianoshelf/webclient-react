68030380ec6a16eb2fb7ab9dc14fd303473b87fe
Ankit Sardesai <amsardesai@gmail.com>
2015-05-28 01:58:19 -0700

    Added browserify to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,40 @@
 
-var gulp = require('gulp');
+var browserify = require('browserify');
 var concat = require('gulp-concat');
-var sourcemaps = require('gulp-sourcemaps');
 var del = require('del');
+var gulp = require('gulp');
+var reactify = require('reactify');
+var source = require('vinyl-source-stream');
 
 var paths = {
-  scripts: 'app/main.jsx'
+  scriptMain: 'app/main.jsx',
+  scripts: 'app/*'
 };
 
 // Not all tasks need to use streams
 // A gulpfile is just another node program and you can use all packages available on npm
-gulp.task('clean', function(cb) {
+gulp.task('clean', function(callback) {
   // You can use multiple globbing patterns as you would with `gulp.src`
-  del(['build'], cb);
+  del(['build'], callback);
 });
 
-gulp.task('scripts', ['clean'], function() {
-  // Minify and copy all JavaScript (except vendor scripts)
-  // with sourcemaps all the way down
-  return gulp.src(paths.scripts)
-    .pipe(sourcemaps.init())
-      .pipe(concat('all.min.js'))
-    .pipe(sourcemaps.write())
+gulp.task('build', ['clean'], function() {
+  var b = browserify();
+  b.transform(reactify);
+  b.add(paths.scriptMain);
+
+  return b.bundle()
+    .pipe(source('app.js'))
     .pipe(gulp.dest('build'));
 });
 
 // Rerun the task when a file changes
 gulp.task('watch', function() {
-  gulp.watch(paths.scripts, ['scripts']);
+  gulp.watch(paths.scripts, ['build']);
 });
 
 // The default task (called when you run `gulp` from cli)
-gulp.task('default', ['watch', 'scripts']);
+gulp.task('default', ['watch', 'build']);
 
 
 
