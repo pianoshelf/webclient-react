f75826faf96648292a77f04d0a017cfcdbefd27c
Ankit Sardesai <amsardesai@gmail.com>
2015-06-01 23:18:07 -0700

    Added babel starting point for server

---
+++ babel.server.js
@@ -0,0 +1,21 @@
+(function() {
+
+  require('babel/register')({
+      stage: 0,
+      plugins: ['typecheck'],
+  });
+
+  // Define isomorphic constants
+  global.__CLIENT__ = false;
+  global.__SERVER__ = true;
+
+  if (process.env.NODE_ENV !== 'production') {
+    if (!require('piping')({hook: true})) {
+      return;
+    }
+  }
+
+  // Launch server
+  require('./app/server');
+
+})();
