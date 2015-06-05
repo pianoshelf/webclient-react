1c4bf8ffbfafb14bdefee88ae020516e45e1aa6a
Ankit Sardesai <amsardesai@gmail.com>
2015-06-05 02:21:38 -0700

    Added a configuration file

---
+++ config.js
@@ -0,0 +1,27 @@
+
+module.exports = {
+
+  servers: {
+    dev: {
+      port: 8000,
+      host: 'localhost',
+    },
+    webpack: {
+      port: 8080,
+      host: 'localhost',
+    },
+  },
+
+
+  api: {
+    prefix: 'https://www.pianoshelf.com/api',
+
+  },
+
+
+  cookie: {
+    authToken: 'pianoshelf-authtoken',
+  },
+
+};
+
