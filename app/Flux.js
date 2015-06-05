6848f45cc4aaa971b3584fa3e7f0ddea4049f17c
Ankit Sardesai <amsardesai@gmail.com>
2015-06-05 02:24:36 -0700

    Attached request object to Flux object

--- app/Flux.js
+++ app/Flux.js
@@ -6,11 +6,23 @@ import { Flummox } from 'flummox';
 export default class Flux extends Flummox {
 
   /**
-   * @param {Express.Request=} req The express server context if in a server context.
+   * @param {Express.Request=} request The express server context if in a server context.
    */
-  constructor(req) {
+  constructor(request) {
     super();
 
+    // Set this instance's request object to the request provided.
+    this.request_ = request;
+
+  }
+
+  /**
+   * Gets the current request object, useful in the server context.
+   *
+   * @return {Express.Request} The current request object.
+   */
+  get request() {
+    return this.request_;
   }
 
 }
