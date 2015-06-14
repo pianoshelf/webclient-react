215e8d87c584b7104e414794ece0f0e554db6052
Ankit Sardesai <amsardesai@gmail.com>
2015-06-14 15:07:37 -0700

    Installed Chai As Promised

--- test/setup.js
+++ test/setup.js
@@ -1,7 +1,12 @@
 'use strict'; // eslint-disable-line strict
 
-// Import JSDOM
+// Import external modules
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
 import { jsdom } from 'jsdom';
+
+// Set up chai as promised
+chai.use(chaiAsPromised);
 
 // Set up global.document variable
 global.document = jsdom('<!doctype html><html><body></body></html>');
