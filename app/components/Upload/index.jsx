90dabd91f1c859f6296b03de2fc858985e8b074a
Ankit Sardesai <amsardesai@gmail.com>
2016-03-16 04:33:34 -0700

    Done most of the visuals of the upload page

--- app/components/Upload/index.jsx
+++ app/components/Upload/index.jsx
@@ -4,8 +4,10 @@ import React from 'react';
 // Import other components
 import Dropzone from './Dropzone';
 import Checkbox from '../Misc/Checkbox';
+import Field from './utils/Field';
 import Input from '../Misc/Input';
 import TextArea from '../Misc/TextArea';
+import Title from './utils/Title';
 import NavBar from '../Fixtures/NavBar';
 import Footer from '../Fixtures/Footer';
 import ResponsiveContainer from '../Misc/ResponsiveContainer';
@@ -15,88 +17,81 @@ export default class Upload extends React.Component {
     children: React.PropTypes.node,
   };
 
+  renderDropzone() {
+    return (
+      <div className="upload__dropzone-wrapper">
+        <Title step={1} text="Upload file" />
+        <Dropzone />
+      </div>
+    );
+  }
+
   renderOptions() {
     return (
       <div className="upload__options">
-        <div className="upload__options-title">
-          Details
-        </div>
-        <div className="upload__options-input-field">
-          <div className="upload__options-input">
+        <Title step={2} text="Enter details" />
+        <div>
+          <Field
+            tip="Enter the title of the sheet music."
+            example={"F\u00FCr Elise"}
+          >
             <Input
               icon="file-o"
               placeholder="Title"
             />
-          </div>
-          <div className="upload__options-input-field-tip">
-            <div className="upload__options-input-field-tip-box">
-              Enter the title of the sheet music.
-            </div>
-          </div>
-        </div>
-        <div className="upload__options-input-field">
-          <div className="upload__options-input">
+          </Field>
+          <Field
+            tip="Enter the original artist or composer who made the music."
+            example="Beethoven"
+          >
             <Input
               icon="user"
               placeholder="Original Artist / Composer"
             />
-          </div>
-          <div className="upload__options-input-field-tip">
-            <div className="upload__options-input-field-tip-box">
-              Enter the original artist or composer who made the music.
-            </div>
-          </div>
-        </div>
-        <div className="upload__options-input-field">
-          <div className="upload__options-input">
+          </Field>
+          <Field
+            tip="Enter the genre of the sheet music."
+            example="Classical"
+          >
             <Input
               icon="music"
               placeholder="Genre"
             />
-          </div>
-          <div className="upload__options-input-field-tip">
-            <div className="upload__options-input-field-tip-box">
-              Enter the genre of the sheet music (i.e Pop or Classical).
-            </div>
-          </div>
-        </div>
-        <div className="upload__options-input-field">
-          <div className="upload__options-input">
+          </Field>
+          <Field
+            tip="Enter a description about this piece."
+            example="This is my favourite sheet music!"
+          >
             <TextArea
+              icon="pencil"
               placeholder="Description"
             />
-          </div>
-          <div className="upload__options-input-field-tip">
-            <div className="upload__options-input-field-tip-box">
-              Enter any extra information about this piece.
-            </div>
-          </div>
-        </div>
-        <div className="upload__options-input-field">
-          <div className="upload__options-input">
+          </Field>
+          <Field
+            tip="Is the sheet music an arrangement of the original?"
+          >
             <Checkbox
               label="Arrangement"
             />
-          </div>
-          <div className="upload__options-input-field-tip">
-            <div className="upload__options-input-field-tip-box">
-              Is the sheet music an arrangement of the original?
-            </div>
-          </div>
-        </div>
-        <div className="upload__options-input-field">
-          <div className="upload__options-input">
+          </Field>
+          <Field
+            tip="Enter the key of the sheet music."
+            example="C major"
+          >
             <Input
-              icon=""
-              placeholder="Genre"
+              icon="key"
+              placeholder="Key"
             />
-          </div>
-          <div className="upload__options-input-field-tip">
-            <div className="upload__options-input-field-tip-box">
-              Enter any extra information about this piece.
-            </div>
-          </div>
+          </Field>
         </div>
+      </div>
+    );
+  }
+
+  renderSubmit() {
+    return (
+      <div className="upload__submit">
+        <Title step={3} text="Submit" />
       </div>
     );
   }
@@ -106,8 +101,9 @@ export default class Upload extends React.Component {
       <div className="upload">
         <NavBar />
         <ResponsiveContainer className="upload__container">
-          <Dropzone />
+          {this.renderDropzone()}
           {this.renderOptions()}
+          {this.renderSubmit()}
         </ResponsiveContainer>
         <Footer />
       </div>
